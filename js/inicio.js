d807acc9ffc3f0128d0a841bb75ec761f260925f
Gines <venazcocabrera@gmail.com>
2016-02-29 18:25:01 +0000

    oki3

--- js/inicio.js
+++ js/inicio.js
@@ -2,6 +2,30 @@ function inicio(){
 	globo();
 	$.mobile.changePage("#page1");
 	//window.location.reload(true);
-	comprobarid();
+	
+	document.addEventListener("deviceready", gcm, false);
+	
+	//comprobarid();
 	
 	}
+	
+	function gcm(){
+		
+		var xregid = localStorage.regid;
+		var xuuid = localStorage.uuid;
+		var xmovil = localStorage.movil;
+	
+	
+	$.ajax({
+		url: 'http://icitacoapp.creatactil.com/php/comprobarid.php',
+		dataType: 'jsonp',
+		jsonp: 'jsoncallback',
+		timeout: 5000,
+		
+		data: {regid: xregid, movil: xmovil, uuid: xuuid},
+		
+			});
+		
+		
+		
+		}
